1c2602a9e982bd642a88a3cc7c23a1196efc7391
Quentin Loader <46482883+QuentinLoader@users.noreply.github.com>
2026-02-23 11:21:39 +0200

    Email Changes - addvison.co.za

--- src/utils/email.js
+++ src/utils/email.js
@@ -7,7 +7,7 @@ export const sendVerificationEmail = async (email, token) => {
   const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
 
   await resend.emails.send({
-    from: "YouScan <youscan.addvision.co.za>",
+    from: "YouScan <addvision.co.za>",
     to: email,
     subject: "Verify your YouScan account",
     html: `
