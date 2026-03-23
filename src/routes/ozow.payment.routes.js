11c38d0bfbdd3e64169b3f1337a9874ff6595a4d
Quentin Loader <46482883+QuentinLoader@users.noreply.github.com>
2026-03-23 12:44:35 +0200

    Payment file update

--- src/routes/ozow.payment.routes.js
+++ src/routes/ozow.payment.routes.js
@@ -44,7 +44,7 @@ router.post("/create-ozow-payment", async (req, res) => {
       ErrorUrl: `${baseUrl}/payment-error`.trim(),
       SuccessUrl: `${baseUrl}/payment-return`.trim(),
       NotifyUrl: `${notifyBase}/ozow`.trim(),
-      IsTest: false // Set to true ONLY if using an Ozow Sandbox SiteCode
+      IsTest: true // Set to true ONLY if using an Ozow Sandbox SiteCode
     };
 
     // Construct Hash String
